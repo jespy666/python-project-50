be0758b3823ea013f51d0d3a61738c2ba1f7ee8f
Georgievsky <jespy.kz@gmail.com>
2023-04-25 16:10:06 +0600

    initial commit

---
+++ Makefile
@@ -0,0 +1,11 @@
+gendiff:
+	poetry run gendiff -h
+
+build:
+	poetry build
+
+publish:
+	poetry publish --dry-run
+
+package-install:
+	python3 -m pip install --user dist/*.whl
---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.'
+    )
+    parser.add_argument('first_file', metavar='first_file', type=str)
+    parser.add_argument('second_file', metavar='second_file', type=str)
+    args = parser.parse_args()
+    print(f'Comparing files: {args.first_file} and {args.second_file}')
+
+
+if __name__ == '__main__':
+    main()
