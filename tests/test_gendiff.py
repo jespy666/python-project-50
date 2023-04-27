1c95700a591e6194d37f1987e62a469dd15fd0a2
Georgievsky <jespy.kz@gmail.com>
2023-04-27 16:40:39 +0600

    add test on flat JSON

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -2,4 +2,9 @@ import pytest
 from gendiff.gendiff import generate_diff
 
 
-
+def test_generate_diff():
+    result = generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json')
+    with open('tests/fixtures/flat_expected.txt', 'r') as f:
+        expected = f.read()
+    assert result == expected
+    assert not result == ''
