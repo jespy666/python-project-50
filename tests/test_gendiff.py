9508fd41fe2da9f9b62c9315cfa7416c6b586621
Georgievsky <jespy.kz@gmail.com>
2023-04-27 18:14:10 +0600

    fix tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,10 +1,11 @@
 import pytest
 from gendiff.gendiff import generate_diff
+import json
 
 
 def test_generate_diff():
-    result = generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json')
-    with open('tests/fixtures/flat_expected.txt', 'r') as f:
+    result = generate_diff('./tests/fixtures/file1.json', './tests/fixtures/file2.json')
+    with open('./tests/fixtures/flat_expected.txt', 'r') as f:
         expected = f.read()
     assert result == expected
     assert not result == ''
