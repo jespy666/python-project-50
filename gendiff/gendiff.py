d94fe033ed9a12267d5efa9166333423a69e1ad9
Georgievsky <jespy.kz@gmail.com>
2023-05-08 14:03:52 +0600

    add "stylish" format name

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,28 +1,19 @@
 from gendiff.parser import parse_data
+from gendiff.diff_builder import build_diff
+from gendiff.formaters.stylish import make_stylish
 
 
-def generate_diff(file_path1: str, file_path2: str) -> str:
+def get_formater(format_name):
+    formater = make_stylish
+    return formater
+
+
+def generate_diff(file_path1: str, file_path2: str,
+                  format_name='stylish') -> str:
     """
-    Function takes paths of both files and return diff between it in str format.
+    Function generate diff between two files in default format - 'stylish'.
     """
-    file1, file2 = parse_data(file_path1, file_path2)
-    keys1 = set(file1.keys())
-    keys2 = set(file2.keys())
-    added_keys = keys2 - keys1
-    removed_keys = keys1 - keys2
-    common_keys = keys1 & keys2
-    changed_keys = {k for k in common_keys if file1[k] != file2[k]}
-    unchanged_key = {k for k in common_keys if file1[k] == file2[k]}
-    diff = '{\n'
-    for k in sorted(added_keys | removed_keys | changed_keys | unchanged_key):
-        if k in added_keys:
-            diff += f'  + {k}: {str(file2[k]).lower()}\n'
-        elif k in removed_keys:
-            diff += f'  - {k}: {str(file1[k]).lower()}\n'
-        elif k in changed_keys:
-            diff += f'  - {k}: {str(file1[k]).lower()}\n'
-            diff += f'  + {k}: {str(file2[k]).lower()}\n'
-        elif k in unchanged_key:
-            diff += f'    {k}: {str(file1[k]).lower()}\n'
-    diff += '}'
-    return diff.strip()
+    data1, data2 = parse_data(file_path1, file_path2)
+    diff = build_diff(data1, data2)
+    formater = get_formater(format_name)
+    return formater(diff)
