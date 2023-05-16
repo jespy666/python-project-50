0eb1a00af0df7a84de2119ec709d2829b0f05cb1
Georgievsky <jespy.kz@gmail.com>
2023-05-16 15:17:25 +0600

    fix lint errors

--- gendiff/parse_args.py
+++ gendiff/parse_args.py
@@ -5,7 +5,7 @@ def parse_args():
     parser = argparse.ArgumentParser(
         description='Compares two configuration files'
                     ' and shows a difference.'
-        )
+    )
     parser.add_argument('first_file', metavar='first_file', type=str)
     parser.add_argument('second_file', metavar='second_file', type=str)
     parser.add_argument('-f', '--format', choices=['stylish', 'plain', 'json'],
