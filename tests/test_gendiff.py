f4cacd0c753c1bc4e24cd08e0bb9e055654e71b6
Georgievsky <jespy.kz@gmail.com>
2023-05-16 14:55:41 +0600

    refactor tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -22,43 +22,26 @@ file1_json = get_fixture_path('file1.json')
 file2_json = get_fixture_path('file2.json')
 file1_yaml = get_fixture_path('file1.yaml')
 file2_yaml = get_fixture_path('file2.yaml')
-nested_file1_json = get_fixture_path('nested_file1.json')
-nested_file2_json = get_fixture_path('nested_file2.json')
-nested_file1_yaml = get_fixture_path('nested_file1.yaml')
-nested_file2_yaml = get_fixture_path('nested_file2.yaml')
-stylish_result = get_fixture_path('flat_stylish.txt')
-stylish_nested_result = get_fixture_path('nested.txt')
-plain_nested_result = get_fixture_path('plain.txt')
-json_nested_result = get_fixture_path('json_view.json')
+nested1_json = get_fixture_path('nested_file1.json')
+nested2_json = get_fixture_path('nested_file2.json')
+nested1_yaml = get_fixture_path('nested_file1.yaml')
+nested2_yaml = get_fixture_path('nested_file2.yaml')
+stylish = get_fixture_path('flat_stylish.txt')
+nested_stylish = get_fixture_path('nested.txt')
+nested_plain = get_fixture_path('plain.txt')
+nested_json = get_fixture_path('json_view.json')
 
 
-@pytest.mark.parametrize('file1, file2, stylish_output', [
-    (file1_json, file2_json, stylish_result),
-    (file1_yaml, file2_yaml, stylish_result),
-    (nested_file1_json, nested_file2_json, stylish_nested_result),
-    (nested_file1_yaml, nested_file2_yaml, stylish_nested_result),
+@pytest.mark.parametrize('file1, file2, output, formatter', [
+    (file1_json, file2_json, stylish, 'stylish'),
+    (file1_yaml, file2_yaml, stylish, 'stylish'),
+    (nested1_json, nested2_json, nested_stylish, 'stylish'),
+    (nested1_yaml, nested2_yaml, nested_stylish, 'stylish'),
+    (nested1_json, nested2_json, nested_plain, 'plain'),
+    (nested1_yaml, nested2_yaml, nested_plain, 'plain'),
+    (nested1_json, nested2_json, nested_json, 'json'),
+    (nested1_yaml, nested2_yaml, nested_json, 'json')
 ])
-def test_stylish(file1, file2, stylish_output):
-    format_name = 'stylish'
-    assert generate_diff(file1, file2, format_name) ==\
-           get_result(stylish_output)
-
-
-@pytest.mark.parametrize('file1, file2, plain_output', [
-    (nested_file1_json, nested_file2_json, plain_nested_result),
-    (nested_file1_yaml, nested_file2_yaml, plain_nested_result),
-])
-def test_plain(file1, file2, plain_output):
-    format_name = 'plain'
-    assert generate_diff(file1, file2, format_name) ==\
-           get_result(plain_output)
-
-
-@pytest.mark.parametrize('file1, file2, json_view', [
-    (nested_file1_json, nested_file2_json, json_nested_result),
-    (nested_file1_yaml, nested_file2_yaml, json_nested_result),
-])
-def test_json_view(file1, file2, json_view):
-    format_name = 'json'
-    assert generate_diff(file1, file2, format_name) ==\
-           get_result(json_view)
+def test_generate_diff(file1, file2, output, formatter):
+    assert generate_diff(file1, file2, formatter) ==\
+           get_result(output)
